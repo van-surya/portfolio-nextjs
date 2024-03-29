967807b83d60c4c3babc738ff34ac5e5c8d7c0ed
van-surya <suryavan08@gmail.com>
2024-03-29 22:40:05 +0700

    change footer

--- src/app/layout/Footer.tsx
+++ src/app/layout/Footer.tsx
@@ -8,7 +8,7 @@ export default function Header() {
         <footer className="w-full py-[2rem] md:py-[3rem] lg:py-[3rem] bg-grad">
             <div className="container mx-auto flex flex-col items-center px-[1rem] md:px-[2rem]">
                 <p className='text-[1rem] md:text-[1.125rem] font-thin text-neutral-900 dark:text-neutral-400 text-center mb-2'>© 2024 Surya. All rights reserved.</p>
-                <p className='text-[1rem] md:text-[1.125rem] font-thin text-neutral-800 dark:text-neutral-500 text-center'><strong className=' text-neutral-800 dark:text-neutral-400 font-medium'>About this website:</strong> built with React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS, Framer Motion, Vercel hosting.</p>
+                <p className='text-[1rem] md:text-[1.125rem] font-thin text-neutral-800 dark:text-neutral-500 text-center'><strong className=' text-neutral-800 dark:text-neutral-400 font-medium'>About this website:</strong> built with React & Next.js, TypeScript, Tailwind CSS, Framer Motion, Vercel hosting.</p>
             </div>
         </footer>
     );

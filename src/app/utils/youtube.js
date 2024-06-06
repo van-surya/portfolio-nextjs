519090fc378b1630c02670df12a5afffc35e8571
Surya <45915194+van-surya@users.noreply.github.com>
2024-06-06 20:33:44 +0700

    Update youtube.js

--- src/app/utils/youtube.js
+++ src/app/utils/youtube.js
@@ -5,9 +5,9 @@ export const fetchYoutubeVideos = async () => {
     const apiKey = process.env.YOUTUBE_API_KEY;
     const response = await axios.get('https://www.googleapis.com/youtube/v3/videos', {
         params: {
-            key: 'AIzaSyAxTaJJvtz2U3XkmBbxbhxM7OhfJ1JwK9g',
+            key: '',
             part: 'snippet',
-            channelId: 'UCy5DLwoFzxEIhB6sbEG1F9g',
+            channelId: '',
             maxResults: 10,
             order: 'date',
         },
